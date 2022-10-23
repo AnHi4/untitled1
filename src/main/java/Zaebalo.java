10a745cdcffe9fe834fdb5e04a2498137bdf9a5f
AnHi4 <an.hi.a.d.1120@gmail.com>
2022-10-23 16:55:38 +0300

    Initial commit

---
+++ src/main/java/Zaebalo.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.*;
+import java.util.ArrayList;
+
+public class Zaebalo {
+    public static void main(String[] args) {
+        // Set HashSet
+        Set<String> set = new HashSet<>();
+        set.add("a");
+        set.add("b");
+        set.add("c");
+
+//       List
+//        List<String> list1 = new ArrayList<>();
+//        System.out.println(list1.size());
+//        System.out.println(list1.isEmpty());
+//        list1.add("a");
+//        list1.add("b");
+//        list1.add("c");
+//        System.out.println(list1);
+//
+//        System.out.println(list1.size());
+//        System.out.println(list1.isEmpty());
+//
+//        System.out.println(list1.contains("x"));
+//
+//        System.out.println(list1.remove("x"));
+//        System.out.println(list1.remove("a"));
+//        System.out.println(list1.remove(1));
+//
+//        System.out.println(list1);
+//        System.out.println(list1.set(0, "xxx"));
+//        System.out.println(list1);
+//        System.out.println(list1.add("abc"));
+//        System.out.println(list1);
+    }
+}
