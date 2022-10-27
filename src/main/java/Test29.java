25ca400365cd7ab10a48639213d52f92dc586831
AnHi4 <an.hi.a.d.1120@gmail.com>
2022-10-27 13:55:20 +0300

    29

--- src/main/java/Test29.java
+++ src/main/java/Test29.java
@@ -5,7 +5,7 @@
 
 public class Test29 {
     public static void main(String[] args) {
-        int nums[] = {1,2,3,4,5,6,7,8};
+        int[] nums = {1,2,3,4,5,6,7,8};
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] ==5){
                 System.out.println("да");
