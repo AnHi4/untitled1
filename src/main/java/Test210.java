99bd2b9161741bcfdb2c34d07b4866c7e24f8a14
AnHi4 <an.hi.a.d.1120@gmail.com>
2022-10-27 13:37:07 +0300

    210

---
+++ src/main/java/Test210.java
@@ -0,0 +1,17 @@
+/*
+Задание: Дан массив с числами. Проверьте, есть ли в нем два одинаковых числа подряд.
+Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
+
+public class Test210 {
+    public static void main(String[] args) {
+        int nums[] = {5, 7, 9, 9, 4};
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] == nums[i+1]){
+                System.out.println("да");
+                break;
+            }
+            else System.out.println("нет");
+        }
+    }
+}
