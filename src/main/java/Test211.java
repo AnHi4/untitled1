dd00a5e9e1c81435a71ca4edef9c9a172b92e154
AnHi4 <an.hi.a.d.1120@gmail.com>
2022-10-27 13:55:44 +0300

    210

--- src/main/java/Test211.java
+++ src/main/java/Test211.java
@@ -5,13 +5,15 @@
 
 public class Test211 {
     public static void main(String[] args) {
-        int nums[] = {3, 2, 4, 2, 4};
+        int[] nums = {3, 2, 2, 2, 2, 4};
         int sum = 0;
+        int j=1;
         for (int i = 0; i < nums.length; i++) {
             sum = sum + nums[i];
-            if (10 > sum) {
-                System.out.println(i + 2);
+            if (sum < 10) {
+                j++;
             }
         }
+        System.out.println(j);
     }
 }
